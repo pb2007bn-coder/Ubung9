c94e56abe831c795916d53afcf803885b44dcd73
pb2007bn-coder <pb2007bn@gmail.com>
2025-12-19 17:57:10 +0100

    Update Passenger.java

--- h2/Passenger.java
+++ h2/Passenger.java
@@ -2,10 +2,10 @@ package h2;
 
 public class Passenger {
   
-  private String name;
-  private int planned;
-  private int visited;
-  private boolean ticket;
+  public String name;
+  public int planned;
+  public int visited;
+  public boolean ticket;
   
   public Passenger(String name, int planned, boolean ticket){
     this.name = name;
